3716e049224d33496689f7cc2a833e3e602afec0
Your Name <you@example.com>
2024-04-02 07:00:48 -0700

    AirBnB_clone_v3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-""" app.py file """
+'''Contains a Flask web application API.
+'''
 import os
-from models import storage
 from flask import Flask, jsonify
-from api.v1.views import app_views
 from flask_cors import CORS
-"""from flasgger import Swagger"""
+
+from models import storage
+from api.v1.views import app_views
+
 
 app = Flask(__name__)
-"""swagger = swagger(app)"""
-db = os.environ.get('HBNB_TYPE_STORAGE', 'json_file')
-host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
-port = os.environ.get('HBNB_API_PORT', '5002')
+'''The Flask web application instance.'''
+app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+app_port = int(os.getenv('HBNB_API_PORT', '5000'))
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-CORS(app, resources={r"/api/": {"origins": "0.0.0.0"}})
+CORS(app, resources={'/*': {'origins': app_host}})
 
 
 @app.teardown_appcontext
@@ -25,9 +26,10 @@ def teardown_flask(exception):
 
 
 @app.errorhandler(404)
-def not_found(e):
-    """error 404"""
-    return jsonify({"error": "Not found"}), 404
+def error_404(error):
+    '''Handles the 404 HTTP error code.'''
+    return jsonify(error='Not found'), 404
+
 
 @app.errorhandler(400)
 def error_400(error):
